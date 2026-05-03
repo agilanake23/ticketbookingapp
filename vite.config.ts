d1fb33bd054cf2c0a1d1ba46c4826b1b86da665c
agilanake23 <agilanake23@gmail.com>
2026-05-03 23:19:32 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/tickebookingapp/",
+  base: "/ticketbookingapp/",
   plugins: [react()],
 })
   
